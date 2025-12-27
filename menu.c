502c9d8653420f23d5739edfc97c215814a3bb41
DamianWiej <vobiscusdamian@gmail.com>
2025-12-27 23:48:48 +0100

    init: zarys menu

---
+++ menu.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "menu.h"
+
+void displayMenu(void)
+{
+    printf("=== Rejestr Biura Zagubionych Rzeczy Multiversum ===\n");
+    printf("1. Wyswietl liste przedmiotow\n");
+}
